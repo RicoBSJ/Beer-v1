163feff0e4c3a34e1bd23e565af8f0e1267c7da1
ricobsj <ricobsj@localhost>
2020-07-01 20:22:31 +0200

    KeepOnServletVersions

--- src/com/example/web/BeerSelect.java
+++ src/com/example/web/BeerSelect.java
@@ -1,7 +1,6 @@
 package com.example.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,18 +8,36 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/BeerSelect")
+@WebServlet( "/BeerSelect" )
 public class BeerSelect extends HttpServlet {
-	
-	private static final long serialVersionUID = 1L;
-       
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("Beer Selection Advice<br>");
-		String c = request.getParameter("color");
-		out.println("<br>Got beer color "+c);
-	}
+    private static final long serialVersionUID = 1L;
 
+    @SuppressWarnings( "rawtypes" )
+    protected void doPost( HttpServletRequest request, HttpServletResponse response )
+            throws ServletException, IOException {
+
+        // First version
+
+        // response.setContentType( "text/html" );
+        // PrintWriter out = response.getWriter();
+        // out.println( "Beer Selection Advice<br>" );
+        // String c = request.getParameter( "color" );
+        // out.println( "<br>Got beer color " + c );
+
+        // Second version
+
+        // String c = request.getParameter( "color" );
+        // BeerExpert be = new BeerExpert();
+        // List result = be.getBrands( c );
+        //
+        // response.setContentType( "text/html" );
+        // PrintWriter out = response.getWriter();
+        // out.println( "Beer Selection Advice<br>" );
+        //
+        // Iterator it = (Iterator) result.iterator();
+        // while ( it.next() ) {
+        // System.out.println( ( "try: " + it.next() ) );
+        // }
+    }
 }
